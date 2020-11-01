4efaa5477b49fd497db9052ebf24cd2742fbaadb
lhb008 <lhb008@users.noreply.github.com>
2020-11-01 03:06:23 +0000

    deploy: d1c0b26a416b1fe74cb6e62de72df555e42dda3b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.16.0-55ad60',
+    VERSION: '1.16.0-d1c0b2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
